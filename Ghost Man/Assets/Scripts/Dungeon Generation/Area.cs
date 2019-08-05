759520e21f08943397161005d667b66f41cf7185
alexamling <alexamling14@gmail.com>
2019-08-04 21:54:37 -0400

    Base Game Upload

---
+++ Ghost Man/Assets/Scripts/Dungeon Generation/Area.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AreaType { spawn, room, hallway };
+
+public class Area : MonoBehaviour
+{
+    public Sprite[][] tiles;
+
+    public AreaType areaType;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// This will be tied to a trigger collider on the area that 
+    /// will activate whatever should happen when the player enters that room
+    /// such as: 
+    ///  - assign enemy navmesh targets
+    ///  - spawn a boss
+    /// </summary>
+    /// <param name="collision"></param>
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
---
+++ Ghost Man/Assets/Scripts/Dungeon Generation/DungeonGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class will be used to generate new floors of the dungeon, randomly populating the floor with rooms, hallways, and more
+/// </summary>
+public class DungeonGenerator : MonoBehaviour
+{
+    public Sprite wallprefab;
+    public Sprite floorprefab;
+
+    public List<Area> areas;
+    [HideInInspector]
+    public Sprite[] tiles;
+
+    [Header("Generation Variables")]
+    public int mapSize;
+    [Space(10)]
+    public int minRoomsCount;
+    public int maxRoomsCount;
+    [Space(10)]
+    public int minRoomsSize;
+    public int maxRoomsSize;
+    [Space(10)]
+    public int minHallwayLength;
+    public int maxHallwayLength;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Ideally will not need to update
+    void Update()
+    {
+        
+    }
+    
+    /// <summary>
+    /// Called once for each floor, past that, the interactions with the floor should be self automating
+    /// </summary>
+    /// <param name="size">size to be used for dungeon width and height</param>
+    public void GenerateNewDungeon(int size)
+    {
+        tiles = new Sprite[size * size];
+    }
+
+    void AddRoom(int xPos, int yPos, int width, int height)
+    {
+
+    }
+
+    void AddHallway(int xStart, int yStary, int length)
+    {
+
+    }
+}
